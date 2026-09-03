a3bfea7e4b91ab3d1feef8065419d5b1df2a2d43
kkang121245 <260236209+kkang121245@users.noreply.github.com>
2026-09-03 14:44:24 +0900

    Add files via upload

---
+++ dictionaries/extension-names.js
@@ -0,0 +1,37 @@
+(() => {
+  const store = (globalThis.__stKoLocalizerDictStore ??= {});
+  store["extension-names"] = {
+  "exactEntries": [
+    [
+      "酒馆助手",
+      "태번 헬퍼"
+    ],
+    [
+      "鸡尾酒",
+      "칵테일"
+    ],
+    [
+      "世界书大扫除🧹",
+      "월드 인포 대청소🧹"
+    ],
+    [
+      "记忆增强表格",
+      "기억 강화 테이블"
+    ],
+    [
+      "Horae - 时光记忆",
+      "Horae - 시간의 기억"
+    ],
+    [
+      "柏宝箱",
+      "바이바이 보물상자"
+    ],
+    [
+      "MiniMax语音",
+      "MiniMax 음성"
+    ]
+  ],
+  "overrideEntries": [],
+  "regexRules": []
+};
+})();
